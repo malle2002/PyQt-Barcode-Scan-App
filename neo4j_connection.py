ae48ebe61ea51418e0a8b3744075dbed127f64c9
malle2002 <93403675+malle2002@users.noreply.github.com>
2025-03-10 18:57:47 +0100

    Update neo4j_connection.py

--- neo4j_connection.py
+++ neo4j_connection.py
@@ -2,7 +2,7 @@ from neo4j import GraphDatabase
 
 NEO4J_URI = "bolt://localhost:7687"
 NEO4J_USER = "neo4j"
-NEO4J_PASSWORD = "ASds12:D"
+NEO4J_PASSWORD = "your_pw"
 
 def get_db_connection():
     """Establish connection to Neo4j database."""
